7c47ea0e9d2207172121ab18c75eddd41bd343e1
5tronciy <62604867+5tronciy@users.noreply.github.com>
2021-04-01 17:53:15 +0300

    main request

---
+++ src/store/form/formSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  visible: false,
+  currentTask: null,
+  edit: false,
+};
+
+export const formSlice = createSlice({
+  name: "form",
+  initialState,
+  reducers: {
+    makeVisible: (state, action) => {
+      state.visible = true;
+      state.currentTask = action.payload;
+      if (action.payload) {
+        state.edit = true;
+      }
+    },
+    makeInvisible: (state) => initialState,
+  },
+});
+
+export const { makeVisible, makeInvisible } = formSlice.actions;
+
+export const selectFormVisibility = (state) => state.form.visible;
+
+export default formSlice.reducer;
