27659a8b6fb07b0627b0156888c400f11e9c2b7f
5tronciy <62604867+5tronciy@users.noreply.github.com>
2021-04-01 17:38:08 +0300

    add Toast from toastify

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
-import TaskList from "./components/TaskList";
+import "./App.css";
+import CustomNavbar from "./components/Navbar";
+import AppRouter from "./router/AppRouter";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className="App">
-      <TaskList />
+      <ToastContainer position="bottom-right" />
+      <CustomNavbar />
+      <AppRouter />
     </div>
   );
 }
