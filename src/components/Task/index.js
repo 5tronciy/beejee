004d5c396f57df19fc25b0e40e3022f336a4178c
5tronciy <62604867+5tronciy@users.noreply.github.com>
2021-04-02 23:31:53 +0300

    add badges to task

--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -4,9 +4,11 @@ import ClampedText from "../ClampedText";
 import { Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { makeVisible } from "../../store/form/formSlice";
+import { ReactComponent as DoneImg } from "../../assets/check.svg";
+import { ReactComponent as EditedImg } from "../../assets/edit.svg";
 
 const Task = ({ task }) => {
-  const { username, email, text } = task;
+  const { username, email, text, status } = task;
   const dispatch = useDispatch();
 
   const loggedIn = useSelector((state) => state.auth.loggedIn);
@@ -14,15 +16,23 @@ const Task = ({ task }) => {
   return (
     <div className={styles.card}>
       <div className={styles.cardShadow}>
-        <div className={styles.content}>
-          <span>Name:</span>
-          <h5 className={styles.userName}>{username}</h5>
+        <div className={styles.userInfo}>
+          <div className={styles.content}>
+            <span>Name:</span>
+            <h5 className={styles.userName}>{username}</h5>
+          </div>
+          <div className={styles.content}>
+            <span>E-mail:</span>
+            <h6 className={styles.email}>{email}</h6>
+          </div>
         </div>
-        <div className={styles.content}>
-          <span>E-mail:</span>
-          <h6 className={styles.email}>{email}</h6>
+        <div className={styles.task}>
+          <div className={styles.badges}>
+            {(status === 10 || status === 11) && <DoneImg />}
+            {(status === 1 || status === 11) && <EditedImg />}
+          </div>
+          <ClampedText className="task-item__text" text={text} />
         </div>
-        <ClampedText className="task-item__text" text={text} />
         {loggedIn && (
           <Button onClick={() => dispatch(makeVisible(task))}>Edit</Button>
         )}
